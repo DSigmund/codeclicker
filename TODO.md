169ddb1d8c662ab08fe8dfa8899509513a5788cc
Dominik Sigmund <dominik.sigmund@br.de>
2020-10-13 13:07:03 +0200

    Basic stuff

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- add item on 10 clicks to buy that doubles addlinesofcode
+
+- add autoclicker
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'codeclicker';
+  linesOfCode = 0;
+  addLinesOfCode = 1;
+  log = [];
+  adjectives = [
+    'beautiful',
+    'nice',
+    'terrific',
+    'bad'
+  ];
+
+  public writeLineOfCode(): void {
+    this.linesOfCode += this.addLinesOfCode;
+    let randomAdjective = this.adjectives[Math.floor(Math.random() * this.adjectives.length)];
+    let sentence = '> You wrote ' + this.addLinesOfCode + ' line' + (this.addLinesOfCode > 1 ? 's' : '') + ' of ' + randomAdjective + ' code.';
+    this.log.push(sentence);
+    document.querySelector("#log li:last-child").scrollIntoView();
+    localStorage.setItem('linesOfCode', this.linesOfCode.toString());
+  }
+
+  ngOnInit() {
+    this.linesOfCode = parseInt(localStorage.getItem('linesOfCode')) | 0;
+    let sentence = '> Loaded ' + this.linesOfCode + ' line' + (this.linesOfCode > 1 ? 's' : '') + ' of code.';
+    this.log.push(sentence);
+  }
 }
